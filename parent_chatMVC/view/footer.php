66ce2168ca30d1ccf08e11dfe95f16311c11d929
lkaymakcioglu <kayleyla42@gmail.com>
2018-03-20 14:24:18 +0100

    first commit !

---
+++ parent_chatMVC/view/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link href="../public/css/style_footer.css" rel="stylesheet" /> 
+</head>
+<body>
+	<div class="footer">
+		<img src="../Images/facebook-logo.png" class="facebook-logo inline">
+		<h1 class="inline"><a href="#">Mentions légales</a></h1>
+	</div>
+</body>
+</html>
---
+++ parent_chatMVC/view/message.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link href="../public/css/style_message.css" rel="stylesheet" /> 
+</head>
+<body>
+	<?php include 'header.php' ?>
+	<?php include 'footer.php' ?>
+</body>
+</html>
