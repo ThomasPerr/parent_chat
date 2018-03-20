00074cd9a6da6510a601148864a84f8c5b42b246
thomas <thomas.perret43@gmail.com>
2018-03-20 14:51:56 +0100

    yes2

--- parent_chatMVC/edit.php
+++ parent_chatMVC/edit.php
@@ -1,9 +1,24 @@
 <?php
 require('model/frontend.php');
 
-if (isset($_GET['idEdit']) && $_GET['idEdit'] > 0) {
-    $comments = editComment($_GET['idEdit']);
-    require('view/postView.php');
+if (isset($_GET['idComment']) && $_GET['idComment'] > 0) {
+    
+    if (isset($_POST) && !empty($_POST['comment'])) {
+        
+        $idComment = $_GET['idComment'];
+        
+        $author = $_POST['author'];
+        
+        $comment = $_POST['comment'];
+        
+        editComment($idComment, $author, $comment);
+        
+        // todo redirect to list of comments
+    }    
+    else {
+        $comment = getComment($_GET['idComment']);
+        require('view/editView.php');
+    }
 }
 else {
     echo 'Erreur : aucun identifiant de billet envoyé';
--- parent_chatMVC/view/editView.php
+++ parent_chatMVC/view/editView.php
@@ -1,17 +1,21 @@
 <?php $title = 'Mon bloc - Billet'; ?>
 
-            <form action="index.php?action=editIfComment&amp;idEdit=<?= $_GET['idEdit'] ?>&idBillet=<?= $_GET['idBillet'] ?>" method="post">
+<?php ob_start(); ?>
+
+            <form action="edit.php?idComment=<?= $_GET['idComment'] ?>" method="post">
                 <div>
                     <label for="author">Auteur</label><br />
-                    <input type="text" id="author" name="author" />
+                    <input type="text" id="author" name="author" value="<?= $comment["author"] ?>" />
                 </div>
                 <div>
                     <label for="comment">Commentaire</label><br />
-                    <textarea id="comment" name="comment"></textarea>
+                    <textarea id="comment" name="comment"><?= $comment["comment"] ?></textarea>
                 </div>
                 <div>
                     <input type="submit" />
                 </div>
             </form>
+            
+<?php $content = ob_get_clean(); ?>            
 
 <?php require('template.php'); ?>
